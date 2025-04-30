4b6a71b8d479f41aa5db7ec0622597587cdec93f
HelMoonflare <alexanderplanton@gmail.com>
2025-04-30 17:37:31 +0200

    sigo?

--- src/main/java/com/alexander/Model/GestorPersonajes.java
+++ src/main/java/com/alexander/Model/GestorPersonajes.java
@@ -21,21 +21,14 @@ public class GestorPersonajes {
         observers.forEach(item -> item.onChange());
     }
 
-<<<<<<< HEAD
-    public CargarPersonajes(){
-=======
+    
     public GestorPersonajes(){
->>>>>>> 263ab297eb86f9b576ad1f2370ef8d42664a8de1
         this.personajes = LecturaEnemigos.leerEnemigos();
         observers = new ArrayList<>();
     }
 
     public void insertarEnemigo(Enemigo e){
-<<<<<<< HEAD
-        this.personaje.add(e);
-=======
         this.personajes.add(e);
->>>>>>> 263ab297eb86f9b576ad1f2370ef8d42664a8de1
         notifyObservers();
     }
 
