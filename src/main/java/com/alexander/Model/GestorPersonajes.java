a8c5e582c0e44acfaa7f543bb371fcd888b2f47a
Rebelan <abelcm2013@gmail.com>
2025-05-06 17:12:45 +0200

    Retoques e ida de olla

--- src/main/java/com/alexander/Model/GestorPersonajes.java
+++ src/main/java/com/alexander/Model/GestorPersonajes.java
@@ -2,46 +2,55 @@ package com.alexander.Model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.alexander.Interfaces.Observer;
 
 public class GestorPersonajes {
+    Protagonista prota;
     ArrayList<Personaje> personajes;
     ArrayList<Observer> observers;
 
-    public void subscribe(Observer observer){
+    public void subscribe(Observer observer) {
         observers.add(observer);
     }
 
-    public void unsubscribe(Observer observer){
+    public void unsubscribe(Observer observer) {
         observers.remove(observer);
     }
 
-    public void notifyObservers(){
+    public void notifyObservers() {
         observers.forEach(item -> item.onChange());
     }
 
-    
-    public GestorPersonajes(){
+    public GestorPersonajes() {
         this.personajes = LecturaEnemigos.leerEnemigos();
         observers = new ArrayList<>();
     }
 
-    public void insertarEnemigo(Enemigo e){
+    public void insertarEnemigo(Enemigo e) {
         this.personajes.add(e);
         notifyObservers();
     }
 
-    public void eliminarEnemigo(Enemigo e){
+    public void eliminarEnemigo(Enemigo e) {
         this.personajes.remove(e);
         notifyObservers();
     }
 
-    public List<Personaje> getNombreEnemigo(){
+    public List<Personaje> getNombreEnemigo() {
         return this.personajes;
     }
 
-
+    public Protagonista getProta() {
+        return prota;
+    }
+    
+    public Enemigo getEnemigo() {
+        Random  random = new Random();
+        return (Enemigo) personajes.get(random.nextInt(personajes.size()));
+    }
+    
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -49,11 +58,6 @@ public class GestorPersonajes {
             sb.append(personaje + "\n");
         }
         return sb.toString();
-    }
-
-    public static void main(String[] args) {
-        GestorPersonajes gp = new GestorPersonajes();
-        System.out.println(gp.toString());
     }
 
 }
--- src/main/java/com/alexander/Model/Tablero.java
+++ src/main/java/com/alexander/Model/Tablero.java
@@ -13,7 +13,7 @@ import com.alexander.App;
 public class Tablero {
     private Casilla[][] tablero;
 
-    public void LecturaInicioTablero(){
+    public void LecturaInicioTablero(GestorPersonajes gp) {
         String[] columnas;
         int filas = 0;
          try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(App.class.getResource("data/tablero.DARKEST").toURI())), StandardCharsets.UTF_8))){
@@ -29,10 +29,10 @@ public class Tablero {
                             tablero[filas][i] = new Casilla(TipoCasilla.Pared, null);
                             break;
                         case 2:
-                            tablero[filas][i] = new Casilla(TipoCasilla.Suelo, null);
+                            tablero[filas][i] = new Casilla(TipoCasilla.Suelo, gp.getProta());
                             break;
                         case 3:
-                            tablero[filas][i] = new Casilla(TipoCasilla.Suelo, null);
+                            tablero[filas][i] = new Casilla(TipoCasilla.Suelo, gp.getEnemigo());
                             break; 
                         default:
                         System.out.println("Error en el formato del tablero");
