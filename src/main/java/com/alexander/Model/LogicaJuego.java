d6e4bf4f2bc62b5924ad06732246124263111950
HelMoonflare <alexanderplanton@gmail.com>
2025-05-10 19:16:17 +0200

    cambios menores en los métodos para el combate y creación de un "prototablero" en la vista secundaria

--- src/main/java/com/alexander/Model/LogicaJuego.java
+++ src/main/java/com/alexander/Model/LogicaJuego.java
@@ -14,36 +14,28 @@ public class LogicaJuego {
         this.tablero = tablero;
     }
 
-<<<<<<< HEAD
-    public void MoverProtagonista() {
-        // Lógica para mover al protagonista
-
-    }
-
-    public boolean atacar(Personaje objetivo, Personaje atacante) {
-        double dmg = 0;
+    public boolean atacar(Personaje objetivo, Personaje atacante, double dmg) {
         try {
             dmg = objetivo.getVitalidad() - atacante.getFuerza() * 0.1;
 
-            System.out.println("El daño del ataque es: " + dmg);
         } catch (ArithmeticException e) {
-            System.out.println("Excepción aritmética. Objetivo con vida 0");
             e.printStackTrace();
         } catch (Exception e) {
-            System.out.println("Excepción genérica");
             e.printStackTrace();
         }
 
         return objetivo.recibirGolpe(dmg);
     }
 
-    public boolean recibirGolpe(Personaje objetivo, Personaje atacante, int dmg) {
+    public boolean recibirGolpe(Personaje objetivo, Personaje atacante) {
         objetivo.vitalidad = objetivo.vitalidad - atacante.getFuerza();
         if (objetivo.vitalidad <= 0) {
             return true;
         } else {
             return false;
-=======
+        }
+    }
+
     public void MoverPersonajes(GestorPersonajes gp, Tablero tab) {
         gp.ordenarPersonajes();
         String op;
@@ -54,55 +46,58 @@ public class LogicaJuego {
                 Protagonista e = (Protagonista) p;
                 while (seguir) {
                     System.out.println("A donde te quieres mover ?\n" +
-                        "1. W = Arriba\n" +
-                        "2. S = Abajo\n" +
-                        "3. A = Izquierda\n" +
-                        "4. D =Derecha");
-                op = sc.nextLine();
-                switch (op) {
-                    case "W":
-                        int nuevaX = p.getCordX()-1;
-                        int nuevaY = p.getCordY();
-                        if (tab.getTipoCasilla(nuevaX, nuevaY) == TipoCasilla.Suelo && tab.EstaCasillaEstaVacia(nuevaX, nuevaY)) {
-                            p.moverse(nuevaX, nuevaY);
-                            seguir = false;
-                            tab.actualizarCasilla(p, nuevaX, nuevaY);
-                        }
-                        break;
-                    case "S":
-                        nuevaX = p.getCordX()+1;
-                        nuevaY = p.getCordY();
-                        if (tab.getTipoCasilla(nuevaX, nuevaY) == TipoCasilla.Suelo && tab.EstaCasillaEstaVacia(nuevaX, nuevaY)) {
-                            p.moverse(nuevaX, nuevaY);
-                            seguir = false;
-                            tab.actualizarCasilla(p, nuevaX, nuevaY);
-                        }
-                        break;
-                    case "A":
-                        nuevaX = p.getCordX();
-                        nuevaY = p.getCordY()-1;
-                        if (tab.getTipoCasilla(nuevaX, nuevaY) == TipoCasilla.Suelo && tab.EstaCasillaEstaVacia(nuevaX, nuevaY)) {
-                            p.moverse(nuevaX, nuevaY);
-                            seguir = false;
-                            tab.actualizarCasilla(p, nuevaX, nuevaY);
-                        }
-                        break;
-                    case "D":
-                        nuevaX = p.getCordX();
-                        nuevaY = p.getCordY()+1;
-                        if (tab.getTipoCasilla(nuevaX, nuevaY) == TipoCasilla.Suelo && tab.EstaCasillaEstaVacia(nuevaX, nuevaY)) {
-                            p.moverse(nuevaX, nuevaY);
-                            seguir = false;
-                            tab.actualizarCasilla(p, nuevaX, nuevaY);
-                        }
-                        break;
-                    default:
-                    System.out.println("Error, introduce una opcion valida");
-                        break;
-                }
+                            "1. W = Arriba\n" +
+                            "2. S = Abajo\n" +
+                            "3. A = Izquierda\n" +
+                            "4. D = Derecha");
+                    op = sc.nextLine();
+                    switch (op) {
+                        case "W":
+                            int nuevaX = p.getCordX() - 1;
+                            int nuevaY = p.getCordY();
+                            if (tab.getTipoCasilla(nuevaX, nuevaY) == TipoCasilla.Suelo
+                                    && tab.EstaCasillaEstaVacia(nuevaX, nuevaY)) {
+                                p.moverse(nuevaX, nuevaY);
+                                seguir = false;
+                                tab.actualizarCasilla(p, nuevaX, nuevaY);
+                            }
+                            break;
+                        case "S":
+                            nuevaX = p.getCordX() + 1;
+                            nuevaY = p.getCordY();
+                            if (tab.getTipoCasilla(nuevaX, nuevaY) == TipoCasilla.Suelo
+                                    && tab.EstaCasillaEstaVacia(nuevaX, nuevaY)) {
+                                p.moverse(nuevaX, nuevaY);
+                                seguir = false;
+                                tab.actualizarCasilla(p, nuevaX, nuevaY);
+                            }
+                            break;
+                        case "A":
+                            nuevaX = p.getCordX();
+                            nuevaY = p.getCordY() - 1;
+                            if (tab.getTipoCasilla(nuevaX, nuevaY) == TipoCasilla.Suelo
+                                    && tab.EstaCasillaEstaVacia(nuevaX, nuevaY)) {
+                                p.moverse(nuevaX, nuevaY);
+                                seguir = false;
+                                tab.actualizarCasilla(p, nuevaX, nuevaY);
+                            }
+                            break;
+                        case "D":
+                            nuevaX = p.getCordX();
+                            nuevaY = p.getCordY() + 1;
+                            if (tab.getTipoCasilla(nuevaX, nuevaY) == TipoCasilla.Suelo
+                                    && tab.EstaCasillaEstaVacia(nuevaX, nuevaY)) {
+                                p.moverse(nuevaX, nuevaY);
+                                seguir = false;
+                                tab.actualizarCasilla(p, nuevaX, nuevaY);
+                            }
+                            break;
+                        default:
+                            System.out.println("Error, introduce una opcion valida");
+                            break;
+                    }
                 }
             }
->>>>>>> main
         }
     }
 }
