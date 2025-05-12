907e8ee1bcd4e52689501ff132c63b2ff5a88678
Rebelan <abelcm2013@gmail.com>
2025-05-12 19:26:36 +0200

    Pereza creada

--- src/main/java/com/alexander/controllers/CrearPersonaje.java
+++ src/main/java/com/alexander/controllers/CrearPersonaje.java
@@ -1,12 +1,54 @@
 package com.alexander.controllers;
 
-import java.io.IOException;
 
-import com.alexander.App;
+
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 public class CrearPersonaje {
 
+    @FXML
+    VBox vbox1;
+
+    @FXML
+    Label L1;
+
+    @FXML
+    GridPane Stats;
+
+    @FXML
+    Label id_nombre;
+
+    @FXML
+    Label id_velocidad;
+
+    @FXML
+    Label id_Vitalidad;
+
+    @FXML
+    Label id_fuerza;
+
+    @FXML
+    TextField textNombre;
+
+    @FXML
+    TextField textVelocidad;
+
+    @FXML
+    TextField textVitalidad;
+
+    @FXML
+    TextField textFuerza;
+
+    @FXML
+    Button comenzar;
+
     
+
+   
 }
