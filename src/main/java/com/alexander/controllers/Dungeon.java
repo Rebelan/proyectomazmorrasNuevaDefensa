78e597fbc0f9fbcd95736838fa59a7b28891ff03
Rebelan <abelcm2013@gmail.com>
2025-05-13 17:10:30 +0200

    errores de imports

--- src/main/java/com/alexander/controllers/Dungeon.java
+++ src/main/java/com/alexander/controllers/Dungeon.java
@@ -13,6 +13,8 @@ import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
